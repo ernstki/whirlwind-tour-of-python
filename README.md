668aa7a02490d69ad9ade0602d602987923db7cf
Kevin Ernst <ernstki@mail.uc.edu>
2018-01-11 16:50:36 -0500

    Add '--curl' option to sequencing results downloader
    - update README to add screenshot of seq_core_downloader.py in action

--- README.md
+++ README.md
@@ -4,19 +4,26 @@ The main repository for this project is:
 
     https://tfwebdev.research.cchmc.org/ern6xv/whirlwind-tour-of-python.git
 
+A "Python 101"-level tour of the language syntax is available in
+[`tour_of_python_syntax.py`][tour].
+
 ## Slides
 
 Slides from the "Python: a quick introduction" presentation (created using
 [reveal.js][revealjs]) may be found in the `slides` subdirectory of this
-repository, and may also be viewed online [here][] (FIXME).
+repository, and may also be viewed online [here][slides].
 
-### What you'll need to get started
+## What you'll need to get started
 
-**FIXME**
-
-* Git (FIXME: maybe also a GUI like Syntevo SmartGit)
-* a code editor like [Atom] or [Sublime Text][st]; _Windows' Notepad or OS X's
-  TextEdit.app are_ not _suitable for this activity_
+* some small bit of command line experience (_e.g._, where to find the
+  Terminal / Command Prompt application for your OS)
+* [Git]
+  * probably already available on macOS / OS X
+  * `sudo apt-get install git` on Debian/Ubuntu or 'sudo yum install git' on
+    CentOS/Fedora/RHEL
+  * optionally, maybe also a GUI like GitHub's or Syntevo SmartGit
+* a code editor like [Atom][] or [Sublime Text][st]; _Windows' Notepad or OS
+  X's TextEdit.app are_ not _suitable for this activity_
 
 Decide on a nice home for your copy of the project. (Git calls this
 a "clone"). I like putting projects in a folder called "devel" in my home
@@ -26,11 +33,39 @@ Windows).
 Then:
 
 ```bash
+# on Windows, this might look like 'cd /d "C:\Users\YOURUSERNAME\dev\stuff"
 cd /to/the/place/where/you/do/dev/stuff
 
 # replace `usr0xy` with your actual CCHMC username
 git clone https://usr0xy@tfwebdev.research.cchmc.org/ern6xv/whirlwind-tour-of-python.git
+
+# install dependencies; use a virtualenv if that's your choice
+cd whirlwind*
+python3 -m venv venv
+# on Windows, I think it's just '.\venv\Scripts\activate'
+source venv/bin/activate
+
+# skip the above two commands and add '--user' if you're OK with installing to
+# your "personal" Python 'site-packages' directory
+pip install -r requirements.txt
+
+# test to see if the 'seq_core_downloader.py' script works
+# on Windows, leave off the './'
+./seq_core_downloader.py --help
 ```
+
+## Sequencing Core downloader
+
+A small-but-complete Python application is included which downloads
+sequencing results from the [CCHMC Sequencing Core web site][seqcore].
+
+You might like to view the source [here on GitLab/Hub][seqsrc].
+
+It has a `--help` option; if you provide the `--with-curl` command line
+option, the results include an authentication cookie which allows you to pipe
+the results directly into `bash` or [`parallel`][gnupar].
+
+![Sample invocation of `seq_core_downloader.py`](slides/img/core_seq_downloader.gif)
 
 ## Getting help
 
@@ -38,8 +73,13 @@ git clone https://usr0xy@tfwebdev.research.cchmc.org/ern6xv/whirlwind-tour-of-py
 
 
 [atom]:      https://atom.io/
+[git]:       https://git-scm.com/downloads
+[gnupar]:    https://www.gnu.org/software/parallel/
 [revealjs]:  https://github.com/hakimel/reveal.js/
-[slides]:    https://grosselab.github.io/python-intro/slides/
+[seqcore]:   https://dna.cchmc.org/www/main.php
+[seqsrc]:    ./seq_core_downloader.py
+[slides]:    https://ernstki.github.io/whirlwind-tour-of-python/
 [st]:        https://www.sublimetext.com/
 [submodule]: https://git-scm.com/book/en/v2/Git-Tools-Submodules
+[tour]:      ./tour_of_python_syntax.py
 [venv]:      https://docs.python.org/3.5/library/venv.html
--- seq_core_downloader.py
+++ seq_core_downloader.py
@@ -1,42 +1,95 @@
 #!/usr/bin/env python3
 # vim: fileencoding=utf-8
+"""
+Fetch download links from the CCHMC Sequencing Core web site
+"""
+import re
+import argparse
 import requests
 import lxml.html
 
-from helpers import auth
+from helpers import auth  # for prompt_auth()
 
 BASE_URL = 'https://dna.cchmc.org/www/'
+CURL_CMD = 'curl -LOJ -C -'  # [silent: -s], follow redirects, use filename
+                             # from header, and resume partial downloads
 
-def get_results_download_links():
+def get_results_download_links(with_curl=True):
     """
-    Log in to the Sequencing Core web site and get a list of download links
+    Log in to the Sequencing Core web site and print list of download links
+
+    If 'with_curl' is True, build curl command lines which include the
+    authentication cookie (which you can pipe through 'bash' or 'parallel')
     """
     
     with requests.Session() as s:
+        # one initial request to the "main" page is necessary to get a PHP
+        # session ID
         r = s.get(BASE_URL + 'main.php')
+
+        # assert that the response code is 2xx (200 series) = "no error"
         assert r.ok
 
+        # auth.prompt_auth() returns a tuple of (username, password)
         (u,p) = auth.prompt_auth()
         r = s.post(BASE_URL + 'logon2.php',
                    data={'username': u, 'password': p})
         assert r.ok
 
+        # make sure the response text has "LDAP login OK" in it; this
+        # assertion will fail if you give invalid credentials because
+        # re.search() will return None
+        assert re.search('LDAP login OK', r.text)
+
+        # this request will have the "authenticated" PHP session ID in it
         r = s.get(BASE_URL + 'main.php')
         assert r.ok
 
+        # fetch the response text as an ElementTree that lxml can manipulate
         html = lxml.html.fromstring(r.text)
 
-        # get all the anchor tags within <ol><li> tags
-        a_elems = html.xpath('//ol/li/a')
+        # get all the <a> (anchor) tags within <ol><li> tags
+        results_anchors = html.xpath('//ol/li/a')
 
-        for results_a in a_elems:
-            r = s.get(BASE_URL + results_a.attrib['href'])
+        for a in results_anchors:
+            r = s.get(BASE_URL + a.attrib['href'])
             assert r.ok
             html = lxml.html.fromstring(r.text)
-            download_a = html.xpath('//a[b[contains(., "FULL DOWNLOAD")]]')[0]
-            print(BASE_URL + download_a.attrib['href'])
+
+            # results download link extracted from the parent <a>'s href
+            # attribute which contains <b>FULL DOWNLOAD</b>; XPath magic
+            # (source: https://stackoverflow.com/a/14631741)
+            result_a = html.xpath('//a[b[contains(., "FULL DOWNLOAD")]]')[0]
+
+            if with_curl:
+                # Python string formatting:
+                # https://docs.python.org/3/library/stdtypes.html#str.format
+                cmd = "{curl} -H 'Cookie: PHPSESSID={sid}' '{url}'".format(
+                          curl=CURL_CMD,
+                          sid=s.cookies['PHPSESSID'],
+                          url=BASE_URL + 'results/' + result_a.attrib['href']
+                      )
+                print(cmd)
+            else:
+                print(BASE_URL + result_a.attrib['href'])
 
 
+# this "main guard" allows you to import this script as a module (from another
+# script or an interactive session), but if you execute the script directly
+# (e.g., ./seq_core_downloader.py), it runs the 'get_results_download_links'
+# function
 if __name__ == '__main__':
-    get_results_download_links()
+    # the magic variable '__doc__' is the module docstring (at the top)
+    parser = argparse.ArgumentParser(
+        description=__doc__.strip(),
+        epilog="A Weirauch Lab production --"
+               " bug reports: https://tf.cchmc.org/s/q1d9g")
+
+    parser.add_argument('-c', '--curl', '--use-curl', '--with-curl',
+                        help="create 'curl' command lines with the "
+                             "authentication cookie already included",
+                        action='store_true', dest='with_curl')
+    options = parser.parse_args()
+    
+    get_results_download_links(options.with_curl)
 
